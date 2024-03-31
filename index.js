763de9abd821c6c391c554eb288dc4c7f1687ab9
samfelix61 <samfelix61@gmail.com>
2024-03-31 11:38:53 +0300

    complete dom modifying assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "This is the champion";
+document.body.append(newHeader);
